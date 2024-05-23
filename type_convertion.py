718ad50fec647ad28d030c4669e356eca8f9abfb
Coby Walsh <cobywalsh7@yahoo.com>
2024-05-23 14:02:47 -0600

    created a new file to work on the basics of type convertion

---
+++ type_convertion.py
@@ -0,0 +1,11 @@
+birth_year = input('Birth Year: ')
+print(type(birth_year))
+age = 2024 - int(birth_year)
+print(type(age))
+print(age)
+
+weight = input('How much do you weight in pounds? ')
+print(type(weight))
+kilos = float(weight) * 0.45359237
+print(type(kilos))
+print(kilos)
